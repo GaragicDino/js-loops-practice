5d90602b0672618bc36e3f72d9df93c4efe2e8b8
GaragicDino <55501270+GaragicDino@users.noreply.github.com>
2019-11-07 10:16:00 -0700

    Add files via upload

---
+++ js-loops-practice/main.js
@@ -0,0 +1,23 @@
+// for (let n = 0; n < 500; n++) {
+//     console.log("I'm so happy!")
+// }
+
+// for (let mult4 = 12; mult4 < 800; mult4 += 4) {
+//     console.log(mult4)
+// }
+
+// for (let oddNum = 55; oddNum >= 11; oddNum -= 2) {
+//     console.log(oddNum)
+// }
+
+// let seriesSum = 0;
+// for (let n = 5; n <= 50; n++) {
+//     seriesSum += n;
+// }
+// console.log("Sum of series is: " + seriesSum);
+
+let seriesSum = 0;
+for (let n = 10; n <= 100; n++) {
+    seriesSum += n;
+}
+console.log("Sum of series is: " + seriesSum);
